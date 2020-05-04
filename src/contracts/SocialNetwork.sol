e1eeee7275ba41ead65d1bdce266fc2ba530f25d
dineshdsv3 <dineshdsv3@gmail.com>
2020-05-05 01:15:59 +0530

    create post Function added to the smart contract

--- src/contracts/SocialNetwork.sol
+++ src/contracts/SocialNetwork.sol
@@ -2,11 +2,20 @@ pragma solidity ^0.5.0;
 
 contract SocialNetwork {
 
+    uint public postCount = 0;
+    mapping(uint=>postCount) public posts;
+
     struct Post {
         uint id;
         string content;
         uint tipAmount;
         address author;
+    }
+
+
+    function createPost(_content) public {
+        postCount++;
+        posts[postCount] = Post(postCount,_content,0,msg.sender)
     }
 
 
