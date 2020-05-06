5366824f0bb8ed746799a0ca94f6c6bf3a976bcb
dineshdsv3 <dineshdsv3@gmail.com>
2020-05-06 06:50:29 +0530

    Tip post function created

--- src/contracts/SocialNetwork.sol
+++ src/contracts/SocialNetwork.sol
@@ -24,4 +24,18 @@ contract SocialNetwork {
         posts[postCount] = Post(postCount, _content, 0, msg.sender);
         emit PostCreated(postCount, _content, 0, msg.sender);
     }
+
+    function tipPost(uint _id) public payable {
+        // Fetch the post and store it in memory
+        Post memory _post = posts[_id];
+        // Fetch the author and store it in memory
+        address payable _author = _post.author;
+        // Transfer money to Author
+        address(_author).transfer(msg.value)
+        // Update the tip amount
+        _post.tipAmount = _post.tipAmount + msg.value;
+        // Update the original post from the memory _post
+        posts[_id] = _post
+    }
+
 }
