37af6e2689dc906aa957ae12bb5e59dd0899fe19
dineshdsv3 <dineshdsv3@gmail.com>
2020-05-07 10:21:30 +0530

    Added accounts to loadBlockchain data

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,46 @@
 import React, { Component } from 'react';
 import logo from '../logo.png';
 import './App.css';
-import Web3 from 'web3'
-
+import Web3 from 'web3';
 
 class App extends Component {
+	state = {
+		account: '',
+		socialNetwork: null,
+		postCount: 0,
+		posts: [],
+	};
 
-  async componentWillMount() {
-    await this.loadWeb3();
-    await this.loadBlockchainData()
-  }
+	async componentWillMount() {
+		await this.loadWeb3();
+		await this.loadBlockchainData();
+	}
 
-  async loadWeb3() {
-    if (window.ethereum) {
-      window.web3 = new Web3(window.ethereum)
-      await window.ethereum.enable()
-    }
-    else if (window.web3) {
-      window.web3 = new Web3(window.web3.currentProvider)
-    }
-    else {
-      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
-    }
-  }
+	async loadWeb3() {
+		if (window.ethereum) {
+			window.web3 = new Web3(window.ethereum);
+			await window.ethereum.enable();
+		} else if (window.web3) {
+			window.web3 = new Web3(window.web3.currentProvider);
+		} else {
+			window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!');
+		}
+	}
 
-  async loadBlockchainData() {
-    const web3 = window.web3
-    const accounts = await web3.eth.getAccounts()
-    console.log(accounts)
-  }
+	async loadBlockchainData() {
+		const web3 = window.web3;
+		const accounts = await web3.eth.getAccounts();
+		console.log(accounts);
+		this.setState({ account: accounts[0] });
+	}
 
-  render() {
-    return (
-      <div>
-       <h1>Social Network DApp</h1>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<h1>Social Network DApp</h1>
+			</div>
+		);
+	}
 }
 
 export default App;
