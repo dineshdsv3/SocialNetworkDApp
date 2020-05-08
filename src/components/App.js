b7af5220081a6960fc285889cbdff8e422f452ac
dineshdsv3 <dineshdsv3@gmail.com>
2020-05-08 15:22:53 +0530

    posts shown on front end

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Web3 from 'web3';
 import Navbar from './Navbar';
 import SocialNetwork from '../abis/SocialNetwork.json';
+import Identicon from 'identicon.js';
 
 class App extends Component {
 	state = {
@@ -41,7 +42,7 @@ class App extends Component {
 			this.setState({ socialNetwork });
 			const postCount = await socialNetwork.methods.postCount().call();
 			this.setState({ postCount });
-			console.log(this.state.postCount)
+			console.log(this.state.postCount);
 			// Load Posts
 			for (var i = 1; i <= postCount; i++) {
 				const post = await socialNetwork.methods.posts(i).call();
@@ -60,6 +61,43 @@ class App extends Component {
 				<Navbar account={this.state.account} />
 				<div className="container-fluid mt-5">
 					<h1>Social Network DApp</h1>
+					<div className="row">
+						<main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '500px' }}>
+							<div className="content mr-auto ml-auto">
+								{this.state.posts.map((post, key) => {
+									return (
+										<div className="card mb-4" key={key}>
+											<div className="card-header">
+												<img
+													className="mr-2"
+													width="30"
+													height="30"
+													src={`data:image/png;base64,${new Identicon(
+														post.author,
+														30
+													).toString()}`}
+												/>
+												<small className="text-muted">{post.author}</small>
+											</div>
+											<ul id="postList" className="list-group list-group-flush">
+												<li className="list-group-item">
+													<p>{post.content}</p>
+												</li>
+												<li key={key} className="list-group-item py-2">
+													<small className="float-left mt-1 text-muted">
+														TIPS:
+														{window.web3.utils.fromWei(post.tipAmount.toString(), 'Ether')}{' '}
+														ETH
+													</small>
+													<button className="btn btn-link btn-sm float-right pt-0">TIP 0.1 ETH</button>
+												</li>
+											</ul>
+										</div>
+									);
+								})}
+							</div>
+						</main>
+					</div>
 				</div>
 			</div>
 		);
